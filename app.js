0b5132cf0f0e25e0ab9618ff5240ab1f5234dea2
Joshua <meyer.josh8@gmail.com>
2021-02-26 18:10:02 -0600

    Create grocery class

---
+++ app.js
@@ -0,0 +1,8 @@
+// GROCERY ITEM CLASS
+class Grocery {
+  constructor(itemName, quantity, perishable){
+    this.itemName = itemName;
+    this.quantity = quantity;
+    this.perishable = perishable;
+  }
+}
