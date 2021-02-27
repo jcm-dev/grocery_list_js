522085c301c61f446b691832ad9f734ee0edcace
Joshua <meyer.josh8@gmail.com>
2021-02-26 18:22:57 -0600

    Create submit event listener

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // GROCERY ITEM CLASS
 class Grocery {
-  constructor(itemName, quantity, perishable){
-    this.itemName = itemName;
+  constructor(groceryItem, quantity, perishable){
+    this.groceryItem = groceryItem;
     this.quantity = quantity;
     this.perishable = perishable;
   }
@@ -9,16 +9,45 @@ class Grocery {
 
 // UI CLASS
 class UI {
-  addGroceryToList(itemName){
+  addGrocery(itemName){
 
   }
   showAlert(){
 
   }
-  deleteGroceryItem(target){
+  deleteGrocery(target){
 
   }
   clearFields(){
-    
+
   }
 }
+
+// EVENT LISTENERS ADD
+document.getElementById('grocery-form').addEventListener('submit', function(e){
+  // grab values from the form and assign them to
+  // local variables
+  const groceryItem = document.getElementById('grocery-item').value,
+        quantity = document.getElementById('quantity').value,
+        perishable = document.getElementById('perishable');
+
+  // instantiate grocery item
+  const grocery = new Grocery(groceryItem, quantity, perishable);
+
+  // instantiate UI
+  const ui = new UI();
+
+  // validate field entry
+  if(groceryItem === '' || quantity === '' || perishable === ''){
+    // alert user of empty text fields
+    ui.showAlert('Please fill in all fields', 'error');
+  } else {
+    // add grocery to list
+    ui.addGrocery(grocery);
+
+    // show completion
+    ui.showAlert('Book Added!', 'success');
+  }
+
+  e.preventDefault();
+});
